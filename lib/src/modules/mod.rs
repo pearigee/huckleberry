3aac122d228436a538ecf78592b5f821a06cca40
pearigee <pearigee@hey.com>
2022-10-15 17:48:43 -0700

    Combine existing modules into a "native" module for simplicity.

--- lib/src/modules/mod.rs
+++ lib/src/modules/mod.rs
@@ -1,25 +1,18 @@
 use crate::env::{Env, EnvRef};
 
 use self::{
-    data::data_module, huckleberry::add_eval_definitions, io::io_module, logic::logic_module,
-    math::math_module, special_forms::special_forms_module,
+    huckleberry::add_eval_definitions, native::native_module, special_forms::special_forms_module,
 };
 
-pub mod data;
 pub mod huckleberry;
-pub mod io;
-pub mod logic;
-pub mod math;
+pub mod native;
 pub mod special_forms;
 pub mod utils;
 
 pub fn core_module() -> EnvRef {
     let env = Env::new().into_ref();
-    env.merge(math_module()).unwrap();
     env.merge(special_forms_module()).unwrap();
-    env.merge(logic_module()).unwrap();
-    env.merge(io_module()).unwrap();
-    env.merge(data_module()).unwrap();
+    env.merge(native_module()).unwrap();
     add_eval_definitions(env.clone_ref());
     env
 }
