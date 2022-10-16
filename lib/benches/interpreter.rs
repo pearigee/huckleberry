ccf877ae35b072f15299f3545aca824eb0c068cf
pearigee <pearigee@hey.com>
2022-10-15 18:37:26 -0700

    Fix fib benchmark.

--- lib/benches/interpreter.rs
+++ lib/benches/interpreter.rs
@@ -1,20 +1,21 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use huckleberry_lib::{env::Env, evaluator::eval_exprs, parser::parse};
+use huckleberry_lib::{env::Env, evaluator::eval};
 
 fn fibonacci_recursion(c: &mut Criterion) {
     let env = Env::with_core_module().into_ref();
     // Preparse the code to restict performance measurement to the interpreter.
-    let exprs = parse(
+    eval(
         "
-        (def fib (fn [n] 
-            (if (< n 2) 1 
-                (+ (fib (- n 1)) (fib (- n 2))))))
-        (fib 20)",
+        (defn fib [n] 
+            (if (lt n 2) 1 
+                (+ (fib (- n 1)) 
+                   (fib (- n 2)))))",
+        env.clone_ref(),
     )
     .unwrap();
 
     c.bench_function("fib_recursion_20", |b| {
-        b.iter(|| eval_exprs(&exprs, env.clone_ref()))
+        b.iter(|| eval("(fib 20)", env.clone_ref()))
     });
 }
 
